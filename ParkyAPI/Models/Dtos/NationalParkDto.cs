b8a0dd7aa99f6192672466ff86322611ef34a372
iamstan13y <iamstan13y@gmail.com>
2021-09-17 19:16:48 +0200

    National Park DTO.

---
+++ ParkyAPI/Models/Dtos/NationalParkDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Models.Dtos
+{
+    public class NationalParkDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime Established { get; set; }
+    }
+}
