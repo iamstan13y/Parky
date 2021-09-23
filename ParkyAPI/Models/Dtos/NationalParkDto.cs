d976df4d19c61833ddd77a94e5a7e2978c96e16d
iamstan13y <iamstan13y@gmail.com>
2021-09-23 21:42:42 +0200

    Set National Park DTO fields to required.

--- ParkyAPI/Models/Dtos/NationalParkDto.cs
+++ ParkyAPI/Models/Dtos/NationalParkDto.cs
@@ -9,7 +9,9 @@ namespace ParkyAPI.Models.Dtos
     public class NationalParkDto
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string State { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime Established { get; set; }
