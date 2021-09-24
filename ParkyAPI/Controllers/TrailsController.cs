fde597c8cedf4f4b09a1f450bdbd0581e9cdb5fc
iamstan13y <iamstan13y@gmail.com>
2021-09-24 15:29:16 +0200

    Seperating out DTOs.

--- ParkyAPI/Controllers/TrailsController.cs
+++ ParkyAPI/Controllers/TrailsController.cs
@@ -71,7 +71,7 @@ namespace ParkyAPI.Controllers
             [ProducesResponseType(StatusCodes.Status404NotFound)]
             [ProducesResponseType(StatusCodes.Status500InternalServerError)]
             [ProducesDefaultResponseType]
-            public IActionResult CreateNationaPark([FromBody] TrailDto trailDto)
+            public IActionResult CreateTrail([FromBody] TrailUpsertDto trailDto)
             {
                 if (trailDto == null)
                 {
@@ -99,7 +99,7 @@ namespace ParkyAPI.Controllers
             [ProducesResponseType(204)]
             [ProducesResponseType(StatusCodes.Status404NotFound)]
             [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-            public IActionResult UpdateTrail(int id, [FromBody] TrailDto trailDto)
+            public IActionResult UpdateTrail(int id, [FromBody] TrailUpsertDto trailDto)
             {
                 if (trailDto == null || id != trailDto.Id)
                 {
