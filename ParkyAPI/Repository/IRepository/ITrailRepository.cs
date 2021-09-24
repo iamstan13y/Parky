14b660395425cc8469dd18cb7822945377d43b5f
iamstan13y <iamstan13y@gmail.com>
2021-09-24 10:51:31 +0200

    Added Trial Repository Interface.

---
+++ ParkyAPI/Repository/IRepository/ITrailRepository.cs
@@ -0,0 +1,21 @@
+﻿using ParkyAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Repository.IRepository
+{
+    public interface ITrailRepository
+    {
+        ICollection<Trail> GetTrails();
+        ICollection<Trail> GetTrailsInNationalPark(int nationalParkId);
+        Trail GetTrail(int trailId);
+        bool TrailExists(string name);
+        bool TrailExists(int id);
+        bool CreateTrail(Trail trail);
+        bool UpdateTrail(Trail trail);
+        bool DeleteTrail(Trail trail);
+        bool Save();
+    }
+}
