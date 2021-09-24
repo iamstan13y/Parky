0c580d92839e58474c3c4009b92435784a98b252
iamstan13y <iamstan13y@gmail.com>
2021-09-24 20:20:06 +0200

    Seperated Trail Upsert DTO - Created Trial Create DTO.

--- ParkyAPI/Models/Dtos/TrailCreateDto.cs
+++ ParkyAPI/Models/Dtos/TrailCreateDto.cs
@@ -7,9 +7,8 @@ using static ParkyAPI.Models.Trail;
 
 namespace ParkyAPI.Models.Dtos
 {
-    public class TrailUpsertDto
+    public class TrailCreateDto
     {
-        public int Id { get; set; }
         [Required]
         public string Name { get; set; }
         [Required]
