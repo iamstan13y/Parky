4ffb5b188e1219ec37fcd1e3ea4984bbd24e820c
iamstan13y <iamstan13y@gmail.com>
2021-09-27 16:17:39 +0200

    Seperated Trail Upsert DTO - Added Trail Update DTO.

---
+++ ParkyAPI/Models/Dtos/TrailUpdateDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using static ParkyAPI.Models.Trail;
+
+namespace ParkyAPI.Models.Dtos
+{
+    public class TrailUpdateDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public double Distance { get; set; }
+        public DifficultyType Difficulty { get; set; }
+        [Required]
+        public int NationalParkId { get; set; }
+    }
+}
