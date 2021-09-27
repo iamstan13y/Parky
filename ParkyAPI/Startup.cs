7c53fcb68b78a8ed5fdf5f685108163b2fbd641e
iamstan13y <iamstan13y@gmail.com>
2021-09-27 22:05:10 +0200

    Versioning API 101.

--- ParkyAPI/Startup.cs
+++ ParkyAPI/Startup.cs
@@ -42,10 +42,10 @@ namespace ParkyAPI
             services.AddAutoMapper(typeof(ParkyMappings));
             services.AddSwaggerGen(options => 
             {
-                options.SwaggerDoc("ParkyOpenAPISpecNP",
+                options.SwaggerDoc("ParkyOpenAPISpec",
                     new OpenApiInfo()
                     {
-                        Title = "Parky API (National Park)",
+                        Title = "Parky API",
                         Version = "1",
                         Description = "The Official Parky API.",
                         Contact = new OpenApiContact()
@@ -61,24 +61,24 @@ namespace ParkyAPI
                         }
                     });
 
-                options.SwaggerDoc("ParkyOpenAPISpecTrails",
-                    new OpenApiInfo()
-                    {
-                        Title = "Parky API Trails",
-                        Version = "1",
-                        Description = "The Official Parky API.",
-                        Contact = new OpenApiContact()
-                        {
-                            Email = "iamstan13y@gmail.com",
-                            Name = "Keith Stanley",
-                            Url = new Uri("https://keith.co.zw")
-                        },
-                        License = new OpenApiLicense()
-                        {
-                            Name = "MIT License",
-                            Url = new Uri("https://en.wikipedia.org/wiki/MIT_License")
-                        }
-                    });
+                //options.SwaggerDoc("ParkyOpenAPISpecTrails",
+                //    new OpenApiInfo()
+                //    {
+                //        Title = "Parky API Trails",
+                //        Version = "1",
+                //        Description = "The Official Parky API.",
+                //        Contact = new OpenApiContact()
+                //        {
+                //            Email = "iamstan13y@gmail.com",
+                //            Name = "Keith Stanley",
+                //            Url = new Uri("https://keith.co.zw")
+                //        },
+                //        License = new OpenApiLicense()
+                //        {
+                //            Name = "MIT License",
+                //            Url = new Uri("https://en.wikipedia.org/wiki/MIT_License")
+                //        }
+                //    });
 
                 var xmlCommentFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var cmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentFile);
@@ -101,8 +101,8 @@ namespace ParkyAPI
             app.UseSwagger();
             app.UseSwaggerUI(options =>
             {
-                options.SwaggerEndpoint("/swagger/ParkyOpenAPISpecNP/swagger.json", "Parky API");
-                options.SwaggerEndpoint("/swagger/ParkyOpenAPISpecTrails/swagger.json", "Parky API");
+                options.SwaggerEndpoint("/swagger/ParkyOpenAPISpec/swagger.json", "Parky API");
+                //options.SwaggerEndpoint("/swagger/ParkyOpenAPISpecTrails/swagger.json", "Parky API");
                 options.RoutePrefix = "";
             });
 
