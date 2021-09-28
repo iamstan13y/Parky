c2204c39355993152a420c185498afdf1e1910b6
iamstan13y <iamstan13y@gmail.com>
2021-09-28 11:03:11 +0200

    Added National Parks Controller Version 2.

---
+++ ParkyAPI/Controllers/NationalParksV2Controller.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Models.Dtos;
+using ParkyAPI.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Controllers
+{
+    public class NationalParksV2Controller : Controller
+    {
+        private readonly INationalParkRepository _npRepo;
+        private readonly IMapper _mapper;
+
+        public NationalParksV2Controller(INationalParkRepository npRepo, IMapper mapper)
+        {
+            _mapper = mapper;
+            _npRepo = npRepo;
+        }
+
+        /// <summary>
+        /// Get list of National Parks.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(List<NationalParkDto>))]
+        public IActionResult GetNationalParks()
+        {
+            var objList = _npRepo.GetNationalParks();
+
+            var objDto = new List<NationalParkDto>();
+
+            foreach (var obj in objList)
+            {
+                objDto.Add(_mapper.Map<NationalParkDto>(obj));
+            }
+            return Ok(objDto);
+        }
+    }
+}
