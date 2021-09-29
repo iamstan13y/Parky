5cb747f2039421be34f5548a7e3111d529526c02
iamstan13y <iamstan13y@gmail.com>
2021-09-29 10:17:21 +0200

    API versioning II - Updated configs in Startup.

--- ParkyAPI/Startup.cs
+++ ParkyAPI/Startup.cs
@@ -2,16 +2,19 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using ParkyAPI.Data;
 using ParkyAPI.ParkyMapper;
 using ParkyAPI.Repository;
 using ParkyAPI.Repository.IRepository;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -47,56 +50,16 @@ namespace ParkyAPI
                 options.ReportApiVersions = true;
             });
 
-            services.AddSwaggerGen(options => 
-            {
-                options.SwaggerDoc("ParkyOpenAPISpec",
-                    new OpenApiInfo()
-                    {
-                        Title = "Parky API",
-                        Version = "1",
-                        Description = "The Official Parky API.",
-                        Contact = new OpenApiContact()
-                        {
-                            Email = "iamstan13y@gmail.com",
-                            Name = "Keith Stanley",
-                            Url = new Uri("https://keith.co.zw")
-                        },
-                        License = new OpenApiLicense()
-                        {
-                            Name = "MIT License",
-                            Url = new Uri("https://en.wikipedia.org/wiki/MIT_License")
-                        }
-                    });
+            services.AddVersionedApiExplorer(options => options.GroupNameFormat = "'v'VVV");
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>,ConfigureSwaggerOptions>();
+            services.AddSwaggerGen();
 
-                //options.SwaggerDoc("ParkyOpenAPISpecTrails",
-                //    new OpenApiInfo()
-                //    {
-                //        Title = "Parky API Trails",
-                //        Version = "1",
-                //        Description = "The Official Parky API.",
-                //        Contact = new OpenApiContact()
-                //        {
-                //            Email = "iamstan13y@gmail.com",
-                //            Name = "Keith Stanley",
-                //            Url = new Uri("https://keith.co.zw")
-                //        },
-                //        License = new OpenApiLicense()
-                //        {
-                //            Name = "MIT License",
-                //            Url = new Uri("https://en.wikipedia.org/wiki/MIT_License")
-                //        }
-                //    });
-
-                var xmlCommentFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                var cmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentFile);
-                options.IncludeXmlComments(cmlCommentsFullPath); 
-            });
             services.AddControllers();
             
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider provider)
         {
             if (env.IsDevelopment())
             {
