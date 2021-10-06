e782686dc39256afecaacf5f44f35fd84855632b
iamstan13y <iamstan13y@gmail.com>
2021-10-06 12:23:40 +0200

    Repository Interface.

---
+++ ParkyWeb/Repository/IRepository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetAsync(string url, int Id);
+        Task<IEnumerable<T>> GetAllAsync(string url);
+        Task<bool> CreateAsync(string url, T objToCreate);
+        Task<bool> UpdateAsync(string url, T objToCreate);
+        Task<bool> DeleteAsync(string url, int Id);
+    }
+}
