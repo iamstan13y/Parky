d23ac19dbc586f9366e88f65f2eac8fe3ff7b18e
iamstan13y <iamstan13y@gmail.com>
2021-10-06 16:37:14 +0200

    National & Trail Repository.

---
+++ ParkyWeb/Repository/IRepository/INationalParkRepository.cs
@@ -0,0 +1,12 @@
+﻿using ParkyWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Repository.IRepository
+{
+    public interface INationalParkRepository : IRepository<NationalPark>
+    {
+    }
+}
---
+++ ParkyWeb/Repository/IRepository/ITrailRepository.cs
@@ -0,0 +1,11 @@
+﻿using ParkyWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Repository.IRepository
+{
+    public interface ITrailRepository : IRepository<Trail>
+    }
+}
