3eb7b53277f006909e13a1d209bde641addda820
iamstan13y <iamstan13y@gmail.com>
2021-10-14 16:53:01 +0200

    Upsert Index Action.

--- ParkyWeb/Controllers/NationalParksController.cs
+++ ParkyWeb/Controllers/NationalParksController.cs
@@ -3,6 +3,7 @@ using ParkyWeb.Models;
 using ParkyWeb.Repository.IRepository;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -35,6 +36,47 @@ namespace ParkyWeb.Controllers
                 return NotFound();
             }
             return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(NationalPark obj)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count > 0)
+                {
+                    byte[] picture = null;
+                    using (var fileStream = files[0].OpenReadStream())
+                    {
+                        using (var memoryStream = new MemoryStream())
+                        {
+                            fileStream.CopyTo(memoryStream);
+                            picture = memoryStream.ToArray();
+                        }
+                    }
+                    obj.Picture = picture;
+                }
+                else
+                {
+                    var objFromDb = await _npRepo.GetAsync(SD.NationalParkAPIPath, obj.Id);
+                    obj.Picture = objFromDb.Picture;
+                }
+                if (obj.Id == 0)
+                {
+                    await _npRepo.CreateAsync(SD.NationalParkAPIPath, obj);
+                }
+                else
+                {
+                    await _npRepo.UpdateAsync(SD.NationalParkAPIPath+obj.Id, obj);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(obj);
+            }
         }
 
         public async Task<IActionResult> GetAllNationalPark()
