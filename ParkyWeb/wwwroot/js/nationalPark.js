79bd7200d0309f2fabdf7fbbc5e5073e4ab099e4
iamstan13y <iamstan13y@gmail.com>
2021-10-14 14:34:11 +0200

    JavaScript to load data table.

---
+++ ParkyWeb/wwwroot/js/nationalPark.js
@@ -0,0 +1,32 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/nationalParks/GetAllNationalPark",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "50px" },
+            { "data": "state", "width": "20px" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                                <a href="nationalParks/Upsert/${data}" class='btn btn-success text-white'
+                                    stlye='cursor:pointer;'> <i class='fa fa-edit'></i></a>
+                                    &nbsp;
+                                <a onclick="Delete(nationalParks/Delete/${data})" class='btn btn-danger text-white'
+                                    stlye='cursor:pointer;'> <i class='fa fa-trash-alt'></i></a>
+                                </div>
+                                `;
+                }, "width": "30%"
+            }
+        ]
+    });
+}
