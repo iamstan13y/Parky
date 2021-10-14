e9897fe0ab4f3d17d125c3ab78dbb150805971dd
iamstan13y <iamstan13y@gmail.com>
2021-10-14 15:15:20 +0200

    Upsert View I.

--- ParkyWeb/SD.cs
+++ ParkyWeb/SD.cs
@@ -8,7 +8,7 @@ namespace ParkyWeb
     public static class SD
     {
         public static string APIBaseUrl = "https://localhost:44317/";
-        public static string NationalParkAPIPath = APIBaseUrl + "api/v1/nationalparks";
-        public static string TrailAPIPath = APIBaseUrl + "api/v1/trails";
+        public static string NationalParkAPIPath = APIBaseUrl + "api/v1/nationalparks/";
+        public static string TrailAPIPath = APIBaseUrl + "api/v1/trails/";
     }
 }
