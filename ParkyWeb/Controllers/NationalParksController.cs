cc3d852e0abfa1e8d64f7d7e7a015cc7a073ff84
iamstan13y <iamstan13y@gmail.com>
2021-10-14 12:01:21 +0200

    Added National Parks controller.

---
+++ ParkyWeb/Controllers/NationalParksController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParkyWeb.Models;
+using ParkyWeb.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Controllers
+{
+    public class NationalParksController : Controller
+    {
+        private readonly INationalParkRepository _npRepo;
+
+        public NationalParksController(INationalParkRepository npRepo)
+        {
+            _npRepo = npRepo;
+        }
+        public IActionResult Index()
+        {
+            return View(new NationalPark() { });
+        }
+
+        public async Task<IActionResult> GetAllNationalPark()
+        {
+            return Json(new { data = await _npRepo.GetAllAsync(SD.NationalParkAPIPath) });
+        }
+    }
+}
