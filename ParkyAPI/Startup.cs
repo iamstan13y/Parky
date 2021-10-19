f67a4186eafb567c11991738991af623a9a705f9
iamstan13y <iamstan13y@gmail.com>
2021-10-19 12:11:11 +0200

    Configuring Secret Key.

--- ParkyAPI/Startup.cs
+++ ParkyAPI/Startup.cs
@@ -55,6 +55,9 @@ namespace ParkyAPI
             services.AddTransient<IConfigureOptions<SwaggerGenOptions>,ConfigureSwaggerOptions>();
             services.AddSwaggerGen();
 
+            var appSettings = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettings);
+
             services.AddControllers();
             
         }
