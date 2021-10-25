7c17aa36b3c7c7d0654fc22e6aba4cec4ed49df3
iamstan13y <iamstan13y@gmail.com>
2021-10-25 08:30:22 +0200

    Adding roles to user.

--- ParkyAPI/Controllers/TrailsController.cs
+++ ParkyAPI/Controllers/TrailsController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ParkyAPI.Models;
@@ -55,6 +56,7 @@ namespace ParkyAPI.Controllers
             [ProducesResponseType(200, Type = typeof(TrailDto))]
             [ProducesResponseType(404)]
             [ProducesDefaultResponseType]
+            [Authorize(Roles = "Admin")]
             public IActionResult GetTrail(int id)
             {
                 var obj = _trailRepo.GetTrail(id);
--- ParkyAPI/Repository/UserRepository.cs
+++ ParkyAPI/Repository/UserRepository.cs
@@ -38,7 +38,8 @@ namespace ParkyAPI.Repository
             {
                 Subject = new ClaimsIdentity(new Claim[]
                 {
-                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role)
                 }),
                 Expires = DateTime.UtcNow.AddDays(7),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
@@ -66,7 +67,8 @@ namespace ParkyAPI.Repository
             User userObj = new User()
             {
                 Username = username,
-                Password = password
+                Password = password,
+                Role = "Admin"
             };
 
             _db.Users.Add(userObj);
